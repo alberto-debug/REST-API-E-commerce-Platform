c6553255442242705beaf1b7440d2d622403222a
Alberto <albertojunior848423803@gmail.com>
2025-05-02 23:07:28 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/entities/User.java
+++ src/main/java/com/alberto/e_commerce/application/entities/User.java
@@ -1,4 +1,31 @@
 package com.alberto.e_commerce.application.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+
+
 }
