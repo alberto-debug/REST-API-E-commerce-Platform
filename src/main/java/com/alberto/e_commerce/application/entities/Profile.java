748792a4795898f42241f87c233bed5ab83c02ce
Alberto <albertojunior848423803@gmail.com>
2025-05-02 23:11:39 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/entities/Profile.java
+++ src/main/java/com/alberto/e_commerce/application/entities/Profile.java
@@ -1,4 +1,36 @@
 package com.alberto.e_commerce.application.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "profiles")
 public class Profile {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints;
+
 }
