bf9e0c8cab93a4375e033c0ca44dc1ffca3da73a
Alberto <albertojunior848423803@gmail.com>
2025-05-02 23:34:03 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/entities/Address.java
+++ src/main/java/com/alberto/e_commerce/application/entities/Address.java
@@ -1,7 +1,6 @@
 package com.alberto.e_commerce.application.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Builder
@@ -13,4 +12,30 @@ import lombok.*;
 @Entity
 @Table(name = "addresses")
 public class Address {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zip")
+    private String zip;
+
+    @Column(name = "state")
+    private String state;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
+
+
+
+
 }
