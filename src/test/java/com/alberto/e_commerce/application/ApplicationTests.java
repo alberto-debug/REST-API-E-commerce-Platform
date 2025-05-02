0e2a83120e9313e9a76376535c6957b14490f46a
Alberto <albertojunior848423803@gmail.com>
2025-05-02 22:51:15 +0300

    first commit

---
+++ src/test/java/com/alberto/e_commerce/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.e_commerce.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
