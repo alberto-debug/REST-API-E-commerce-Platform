f05b29a48272c838f07e644ccef315ba5c865028
Alberto <albertojunior848423803@gmail.com>
2025-05-02 23:28:11 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/entities/Address.java
+++ src/main/java/com/alberto/e_commerce/application/entities/Address.java
@@ -1,7 +1,16 @@
 package com.alberto.e_commerce.application.entities;
 
-import lombok.Builder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
 
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
+@Table(name = "addresses")
 public class Address {
 }
