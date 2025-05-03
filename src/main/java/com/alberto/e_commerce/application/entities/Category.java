644dfccbf8502e0a316118c681c0f0875c9c1d83
Alberto <albertojunior848423803@gmail.com>
2025-05-03 16:05:39 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/entities/Category.java
+++ src/main/java/com/alberto/e_commerce/application/entities/Category.java
@@ -15,7 +15,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
+    @Column(name = "id")
     private Byte id;
 
     @Column(name = "name")
