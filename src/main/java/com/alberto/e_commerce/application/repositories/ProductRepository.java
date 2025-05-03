f0253fc3337d73a9bb25b9051bf2241022912d77
Alberto <albertojunior848423803@gmail.com>
2025-05-03 16:27:33 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/repositories/ProductRepository.java
+++ src/main/java/com/alberto/e_commerce/application/repositories/ProductRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alberto.e_commerce.application.entities.Product;
 
-public interface ProductRepository extends JpaRepository<Long, Product> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
