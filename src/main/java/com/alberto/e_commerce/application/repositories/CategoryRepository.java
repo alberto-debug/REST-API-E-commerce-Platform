684cf0740bfbbb9ecd8caf2b106fe1a3aa841065
Alberto <albertojunior848423803@gmail.com>
2025-05-03 16:27:30 +0300

    changes

--- src/main/java/com/alberto/e_commerce/application/repositories/CategoryRepository.java
+++ src/main/java/com/alberto/e_commerce/application/repositories/CategoryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.alberto.e_commerce.application.entities.Category;
 
-public interface CategoryRepository extends CrudRepository<Long, Category> {
+public interface CategoryRepository extends CrudRepository< Category, Byte> {
 }
